2c8bdbf255afd7518f38eafa3811a1d112aafcfc
guillaumejparis <guillaumejparis@users.noreply.github.com>
2019-10-28 21:10:38 +0000

    Automated deployment to GitHub Pages on 1572297038

---
+++ precache-manifest.f43ae7827709490b6a980df9edbcc94b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5009fb9b394d58cd3fd40eab66675244",
+    "url": "/capsule/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "ab8f782a9bbc9ba2bef02d596bc361bf",
+    "url": "/capsule/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a21badbf92ab0e348b8a8c59d4408ba0",
+    "url": "/capsule/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "f46874181ec462401148f5e68a24c30f",
+    "url": "/capsule/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "6b650bd32eaa4b2d9ece7525382248e1",
+    "url": "/capsule/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "e60774b6f5253e6d2e53ac92c6a884a1",
+    "url": "/capsule/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "8c413911e9a21c88f60c138a9a3df5c0",
+    "url": "/capsule/capsule.png"
+  },
+  {
+    "revision": "1f848ad84da09c6f2d3635cae1198e49",
+    "url": "/capsule/favicon.ico"
+  },
+  {
+    "revision": "6e799fc4061d170327bf3d1e888855cd",
+    "url": "/capsule/index.html"
+  },
+  {
+    "revision": "f7a50bc4812e8c7eaf51",
+    "url": "/capsule/main.f7a50bc4.js"
+  },
+  {
+    "revision": "9f601c457afd313e24cf5e10ba09a2fd",
+    "url": "/capsule/manifest.json"
+  }
+]);
